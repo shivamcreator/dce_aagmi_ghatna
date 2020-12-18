02c00a64d819c62745613ccc4b38bcf85767531d
shivamcreator <shivamr.kgoyal@gmail.com>
2020-12-18 11:24:59 +0530

    Add files via upload

---
+++ Aagmi ghatna.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup # BeautifulSoup is in bs4 package 
+import requests
+import webbrowser
+import pyfiglet
+import time
+from random import randint
+from yaspin import yaspin, Spinner
+from printy import printy
+
+URL = 'https://ggnindia.dronacharya.info/upcomingevents.aspx'
+content = requests.get(URL)
+soup = BeautifulSoup(content.text, 'html.parser')
+contentDiv  = soup.find('div', { "class" : "panel-body newsbody3"})
+notices=contentDiv.find_all('a')
+list1=[]
+for n in notices:
+  list1.append(n.get_text())
+
+project_href = [i['href'] for i in contentDiv.find_all('a', href=True)]
+# for a in announcements:
+#   print(a.get_text())
+#
+mapped={}
+
+keys=[]
+for i in range(1,len(list1)+1):
+  keys.append(i)
+
+for key in keys:
+   for value in project_href:
+     mapped[key]=value
+     project_href.remove(value)
+     break
+x=1
+
+banner=pyfiglet.figlet_format("DCE  AAGMI  GHATNA")
+
+printy(banner,"r")
+for i in list1:
+
+  print(str(x)+":",i)
+  x=x+1
+print()
+result = pyfiglet.figlet_format("Choose from above options", font = "digital" ) 
+printy(result,"y")
+while True:
+ enter=int(input("Enter the option:"))
+
+ if(enter in keys):
+  
+  with yaspin(text="Loading", color="yellow") as spinner:
+   time.sleep(4)  # time consuming code
+  webbrowser.open_new("https://ggnindia.dronacharya.info/" + mapped[enter])
+
+
+ else:
+ 
+  print("Enter the correct option")
